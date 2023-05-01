cba5457687f30e3f25f6021dbda66dc7c9591947
Dao <73803690+daotame@users.noreply.github.com>
2023-05-01 09:28:04 -0400

    Update 3tac.py

--- 3tac.py
+++ 3tac.py
@@ -1,18 +1,6 @@
-import re, os, sys
-from lex import file_name, file_name_2
-from parse import file_name_3
-from semantics import file_name_4
+import re
+from lex import file_name
 
-
-if os.path.getsize(file_name_2) != 0:
-    print("Lexical Errors, exiting program")
-    sys.exit()
-if os.path.getsize(file_name_3) != 0:
-    print("Syntax Errors, exiting program")
-    sys.exit()
-if os.path.getsize(file_name_4) != 0:
-    print("Semantics Errors, exiting program")
-    sys.exit()
 
 print("\n")
 print("Code Generation:")
@@ -20,6 +8,11 @@ print("Code Generation:")
 with open(file_name, "r") as f:
     file_contents = f.read()
 
+#print(file_contents)
+#lines = file_contents.split("\n")
+#print(lines)
+#for line in lines:
+#    print(line)
 tac = []
 label_count = 0
 temp_count = 0
@@ -32,7 +25,7 @@ binary_pattern = re.compile(r"(.+)\s*([+\-*/])\s*(.+)")
 call_pattern = re.compile(r"print\s+(\w+)\(([^)]*)\)")
 while_pattern = re.compile(r"while\((.+)\)\s*do")
 print_pattern = re.compile(r"print\((\w+)\)")
-if_pattern = re.compile(r"if\((.+)\)\s*then")
+if_pattern = re.compile(r"if\s*\((.+?)\)\s*then\s*([^}]+?)(?:else\s*([^}]+?))?\s*fi")
 return_pattern = re.compile(r"return\s*\((.+)\)")
 label_pattern = re.compile(r"L(\d+)")
 
@@ -41,16 +34,18 @@ tac = []
 label_count = 0
 temp_count = 0
 
+
 # Parse function declarations
-declarations = declaration_pattern.findall(file_contents)
-for d in declarations:
-    name, params, body = d
-    param_list = [p.strip() for p in params.split(",") if p.strip()]
-    tac.append(f"function {name}({', '.join(param_list)})")
-    body_statements = [s.strip() for s in body.split(";") if s.strip()]
-    for s in body_statements:
+if "def" in file_contents:
+    function_name = file_contents.split("(")[0].split()[-1]
+    function_params = file_contents.split("(")[1].split(")")[0].split(",")
+    #print(function_name)
+    #print(function_params)
+    tac.append(f"function {function_name}{function_params}")
+    statements = [s.strip() for s in file_contents.split(";") if s.strip()]
+    for s in statements:
         # If statements
-        im = if_pattern.match(s)
+        im = if_pattern.search(s)
         if im:
             label_count += 1
             condition = im.group(1)
@@ -59,13 +54,94 @@ for d in declarations:
             else_label = f"L{label_count+1}"
             tac.append(f"goto {else_label}")
             tac.append(f"{then_label}:")
-        # Return statements
-        rm = return_pattern.match(s)
-        if rm:
-            value = rm.group(1)
-            tac.append(f"return {value}")
-    tac.append("end")
+            # Parse the then block
+            for sub_s in s[im.end():].split(";"):
+                sub_s = sub_s.strip()
+                # Check for return statements
+                rm = return_pattern.search(sub_s)
+                if rm:
+                    value = rm.group(1)
+                    tac.append(f"return {value}")
+            tac.append(f"goto L{label_count+2}")
+            tac.append(f"{else_label}:")
+            # Parse the else block
+            for sub_s in s[im.end():].split(";"):
+                sub_s = sub_s.strip()
+                # Check for return statements
+                rm = return_pattern.match(sub_s)
+                if rm:
+                    value = rm.group(1)
+                    tac.append(f"return {value}")
+            tac.append(f"L{label_count+2}:")
+            continue
 
+
+# Parse declarations
+declarations = declaration_pattern.findall(file_contents)
+for d in declarations:
+    for var in d:
+        tac.append(f"declare {var}")
+
+def parse_statements():
+    # Parse statements
+    temp_count = 0
+    label_count = 0
+    statements = [s.strip() for s in file_contents.split(";") if s.strip()]
+    for s in statements:
+        # Assignments
+        m = assignment_pattern.match(s)
+        if m:
+            dest, expr = m.groups()
+            # Check if the expression is binary
+            bm = binary_pattern.match(expr)
+            if bm:
+                op1, op, op2 = bm.groups()
+                temp = f"t{temp_count}"
+                tac.append(f"{op1} = {temp}")
+                tac.append(f"{temp} = {op2} {op} {dest}")
+                temp_count += 1
+            else:
+                tac.append(f"{dest} = {expr}")
+        # While loops
+        m = while_pattern.match(s)
+        if m:
+            label_count += 1
+            start_label = f"L{label_count}"
+            end_label = f"L{label_count+1}"
+            condition = m.group(1)
+            tac.append(f"{start_label}:")
+            # Check if the condition is binary
+            bm = binary_pattern.match(condition)
+            if bm:
+                op1, op, op2 = bm.groups()
+                tac.append(f"if {op1} {op} {op2} goto {end_label}")
+            else:
+                tac.append(f"if not {condition} goto {end_label}")
+            # Loop body
+            for sub_s in s[m.end():].split(";"):
+                sub_s = sub_s.strip()
+                m = assignment_pattern.match(sub_s)
+                if m:
+                    dest, expr = m.groups()
+                    # Check if the expression is binary
+                    bm = binary_pattern.match(expr)
+                    if bm:
+                        op1, op, op2 = bm.groups()
+                        temp = f"t{temp_count}"
+                        tac.append(f"{op1} = {temp}")
+                        tac.append(f"{temp} = {op2} {op} {dest}")
+                        temp_count += 1
+                    else:
+                        tac.append(f"{dest} = {expr}")
+            tac.append(f"goto {start_label}")
+            tac.append(f"{end_label}:")
+        # Print statements
+        m = print_pattern.match(s)
+        if m:
+            tac.append(f"print {m.group(1)}")
+
+
+parse_statements()
 # Parse function call
 call = call_pattern.search(file_contents)
 if call:
@@ -78,67 +154,6 @@ if call:
     temp_var = f"t{temp_count}"
     tac.append(f"{temp_var} = return")
     tac.append(f"print {temp_var}")
-
-# Parse declarations
-declarations = declaration_pattern.findall(file_contents)
-for d in declarations:
-    for var in d:
-        tac.append(f"declare {var}")
-
-# Parse statements
-statements = [s.strip() for s in file_contents.split(";") if s.strip()]
-for s in statements:
-    # Assignments
-    m = assignment_pattern.match(s)
-    if m:
-        dest, expr = m.groups()
-        # Check if the expression is binary
-        bm = binary_pattern.match(expr)
-        if bm:
-            op1, op, op2 = bm.groups()
-            temp = f"t{temp_count}"
-            tac.append(f"{op1} = {temp}")
-            tac.append(f"{temp} = {op2} {op} {dest}")
-            temp_count += 1
-        else:
-            tac.append(f"{dest} = {expr}")
-    # While loops
-    m = while_pattern.match(s)
-    if m:
-        label_count += 1
-        start_label = f"L{label_count}"
-        end_label = f"L{label_count+1}"
-        condition = m.group(1)
-        tac.append(f"{start_label}:")
-        # Check if the condition is binary
-        bm = binary_pattern.match(condition)
-        if bm:
-            op1, op, op2 = bm.groups()
-            tac.append(f"if {op1} {op} {op2} goto {end_label}")
-        else:
-            tac.append(f"if not {condition} goto {end_label}")
-        # Loop body
-        for sub_s in s[m.end():].split(";"):
-            sub_s = sub_s.strip()
-            m = assignment_pattern.match(sub_s)
-            if m:
-                dest, expr = m.groups()
-                # Check if the expression is binary
-                bm = binary_pattern.match(expr)
-                if bm:
-                    op1, op, op2 = bm.groups()
-                    temp = f"t{temp_count}"
-                    tac.append(f"{op1} = {temp}")
-                    tac.append(f"{temp} = {op2} {op} {dest}")
-                    temp_count += 1
-                else:
-                    tac.append(f"{dest} = {expr}")
-        tac.append(f"goto {start_label}")
-        tac.append(f"{end_label}:")
-    # Print statements
-    m = print_pattern.match(s)
-    if m:
-        tac.append(f"print {m.group(1)}")
 
 # Print TAC
 for i, t in enumerate(tac):
